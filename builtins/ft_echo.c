a64ebebff679fec58911e86b1d192c0f3345b435
hceviz <hasan.cvz0@gmail.com>
2025-06-30 11:56:10 +0200

    echo finished

--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -6,50 +6,35 @@
 /*   By: hceviz <hceviz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 10:38:33 by hceviz            #+#    #+#             */
-/*   Updated: 2025/06/29 11:56:51 by hceviz           ###   ########.fr       */
+/*   Updated: 2025/06/30 11:55:27 by hceviz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-/*
-	if there is expansion variable somewhere
-	in the command->fcmd and if it is not exist expansion,
-	and if there is command, exrecute the command
-
-	for ex: $abcdef echo abc -> abc
-	for ex: echo abc $abcdef -> abc
-	for ex: echo $abcdef abc -> abc
-	so it replaces nothing to $abcdef cuz
-	it is not exist.
-	
-	try with echo -$abcdefd
-
-*/
-
-void	handle_n_flag(t_node *command, int *i, int *w_n)
+void handle_n_flag(t_node *command, int *i, int *w_n)
 {
+    int pos; 
+	
+	pos = *i;
+    while (command->fcmd[pos] == ' ')
+        pos++;
+    while (command->fcmd[pos] == '-' && command->fcmd[pos + 1] == 'n')
+    {
+        int j = pos + 2;
+        while (command->fcmd[j] == 'n')
+            j++;
 
-	//echo -n fdgdfgd
-	if (command->fcmd[5] == '-' && command->fcmd[6] == 'n')
-	{
-		if (command->fcmd[7] && command->fcmd[7] != ' ' && command->fcmd[7] != '\0')
-			return ;
-		// if (command->fcmd[7] == ' ' ||)
-		else
-		{
-			*i = 7;
-			while (command->fcmd[*i + 1] == '-'
-				&& command->fcmd[*i + 2] == 'n' 
-				&& (command->fcmd[*i + 3] == ' '
-			|| command->fcmd[*i + 3] == '\0'))
-			{
-				*i += 3;
-				
-			}
-			*w_n = 1;
-		}
-	}
+        if (command->fcmd[j] != ' ' && command->fcmd[j] != '\0')
+            break;
+
+        *w_n = 1;
+        pos = j;
+        while (command->fcmd[pos] == ' ')
+            pos++;
+    }
+
+    *i = pos;
 }
 
 void	ft_echo(t_node *command)
